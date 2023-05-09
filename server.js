4c3933ef4f2d96db0d34bcf764887d8a52103e7b
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-05-09 11:22:01 +0530

    Insert data to database

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 // Importing the express module
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+let db;
+let dbName = process.env.dbName
+let connectionString = process.env.MONGO_URI
+
+// Establishing database connection
+MongoClient.connect(connectionString, {
+    useUnifiedTopology: true
+})
+.then(client => {
+    console.log(`Connected to database`)
+    db = client.db(dbName)
+})
+.catch(error => console.error(error))
 
 // Assigning express object to a variable named `app`.
 // We will call the express properties and methods using `app`.
@@ -15,6 +33,23 @@ app.use(express.json());
 app.get('/admin-panel', (req, res) => {
     res.sendFile(__dirname + '/admin/index.html');
 });
+
+app.post('/admin-panel/add', (req, res) => {    
+
+    db.collection('dish').insertOne({
+        dishname: req.body.dishname,
+        imgurl: req.body.imgurl,
+        country: req.body.country,
+    })
+    .then(result => {
+        console.log('Data added successfully!')
+        res.redirect('/')
+    })
+    .catch(err =>{
+        console.error(err)        
+    })
+    
+})
 
 // Assigning port for server
 const PORT = 5000;
